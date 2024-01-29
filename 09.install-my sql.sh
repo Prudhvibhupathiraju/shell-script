b5835d5089673486866c9b4b88eec7d421606eef
unknown <prudhvirajubh999@gmail.com>
2024-01-29 16:18:14 +0530

    09.install-my sql.sh commit 3

--- 09.install-my sql.sh
+++ 09.install-my sql.sh
@@ -11,3 +11,21 @@ echo "Runnig with root access"
 fi
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+echo "ERROR :: Installation failed"
+exit 1
+else
+echo "Installation success"
+fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+echo "ERROR :: Installation failed"
+exit 1
+else
+echo "Installation success"
+fi
