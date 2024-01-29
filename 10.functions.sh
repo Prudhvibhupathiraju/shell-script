010d7d5899cc3a640acf6251262192f4ca69b8bc
unknown <prudhvirajubh999@gmail.com>
2024-01-29 16:55:24 +0530

    10.functions.sh commit 1

---
+++ 10.functions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then
+echo "ERROR :: Installation $2 failed"
+exit 1
+else
+echo "Installation $2 success"
+fi
+}
+
+if [ $ID -ne 0 ]
+then
+echo "ERROR :: Please run this with root access"
+exit 1
+else
+echo "Runnig with root access"
+fi
+
+yum insatll mysql -y
+
+VALIDATE $? "mysql"
+
+yum install git -y
+
+VALIDATE $? "git"
+
+
+
