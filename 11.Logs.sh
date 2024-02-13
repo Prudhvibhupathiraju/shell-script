315c6705e01583cb2ceabaff730c4d6ce32e3333
unknown <prudhvirajubh999@gmail.com>
2024-02-13 14:39:28 +0530

    11.logs.sh commit 9

--- 11.Logs.sh
+++ 11.Logs.sh
@@ -5,23 +5,26 @@ ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE(){ 
    if [ $1 -ne 0 ]
     then
-    echo "$2 installation --> Failed"
+    echo "$2 installation --> $R Failed $N"
     exit 1
   else
-    echo "$2 installation --> Success"
+    echo "$2 installation --> $G Success $N"
    fi
 }
 
 if [ $ID -ne 0 ]
 then
- echo "ERORR :: Please run this with root access"
+ echo "$R ERORR :: Please run this with root access $N"
  exit 1
  else
- echo "Running  with root access"
+ echo "Running  with $G ROOT $N access"
  fi
 
 yum install mysql -y &>> $LOGFILE
