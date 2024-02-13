b5b940484faafefc9448f2bf3f4c99e2e7785cb2
unknown <prudhvirajubh999@gmail.com>
2024-02-13 13:39:53 +0530

    11.logs.sh commit 1

---
+++ 11.Logs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){ 
+if( $1 -ne 0 )
+then
+echo "$2 installation --> Failed"
+else
+echo "$2 installation --> Success"
+}
+
+if [ $ID -ne 0 ]
+then
+ echo "ERORR :: Please run this with root access"
+ exit 1
+ else
+ echo "Running  with root access"
+ fi
+
+yum install mysql -y
+
+VALIDATE $? "MySQL"
+
+yum install git -y
+
+VALIDATE $? "GIT"
