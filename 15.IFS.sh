46e8e2037d82b3a8383cbd62b8a6982a4f5cd6a1
unknown <prudhvirajubh999@gmail.com>
2024-02-21 14:13:27 +0530

    15 C4

--- 15.IFS.sh
+++ 15.IFS.sh
@@ -14,6 +14,6 @@ then
 
 while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
 do
-echo -e "User Name:$G$username$N/Group_id:$G$group_id$N/User ID:$G$user_id$N/User_fullname:$G$user_fullname$N/Home directory:$G$home_dir$N/Shell_path:$G$shell_path$N"
+echo -e "User Name:$G$username$N/User_fullname:$G$user_fullname$N/Home directory:$G$home_dir$N/Shell_path:$G$shell_path$N"
 
 done < $file
