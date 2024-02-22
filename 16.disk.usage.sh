f3648d7e2a68eb2eb4a038cb1c01dde0b619f839
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2024-02-22 20:29:36 +0530

    16.disk.usage.sh C4

--- 16.disk.usage.sh
+++ 16.disk.usage.sh
@@ -10,7 +10,7 @@ usage=$( echo $line | awk '{print $6F}' | cut -d % f1 )
 partition=$$( echo $line | awk '{print $1F}')
 if [ $usage -ge $disk_threshold ]
 then
-message+=$(echo "High disk usage on $partition : $usage")
+message+="High disk usage on $partition : $usage"
 fi
 done <<< $disk_usage
 
