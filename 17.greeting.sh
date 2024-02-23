3a3b852a70221a4c9363e7426be14fbd731f5d8b
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2024-02-24 02:13:35 +0530

    Update 17.greeting.sh

--- 17.greeting.sh
+++ 17.greeting.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-person=$1
+Names=""
+Wishes=""
 
-echo "Hey $person, I am learing DevOps"
+usage(){
+echo "Usage :: $(basename $0) -n <name> -w<wishes>"
+echo "Options ::"
+echo "-n specify the name(mandatory)"
+echo "-n specify the wishes(mandatory)"
+echo "-h display help"
+
+while getops "n:w:h" opt;
+do
+case $opt in
+n) Name="$OPTARG";;
+w) Wishes="$OPTARG";;
+/?) echo "invalid option : -"$OPTARG"" >&2; usage; exit;; 
+h|*) usage exit;;
+esac
+done
+
+if [ -z "$Name" ] || [ -z "$Wishes" ]
+then
+echo "Error :: Options '-n' and '-w' should not be empty"
+usage
+exit 1
+fi
+
+echo "Hey $Name, $Wishes"
